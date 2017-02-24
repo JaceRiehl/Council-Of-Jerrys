67793cfd28806edf3f307a6518fe9cdc19697a98
Jace Riehl <jace.riehl@gmail.com>
2017-02-24 12:33:26 -0700

    Actually adding Situations

---
+++ include/Situation.h
@@ -0,0 +1,18 @@
+#ifndef SITUATION_H
+#define SITUATION_H
+
+#include<string>
+#include<map>
+
+class Situation
+{
+    public:
+
+
+    private:
+         string jerry = "my name is jerry";
+
+
+
+};
+#endif //SITUATION_H
