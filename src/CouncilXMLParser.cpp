87fdd5d946518a855cf346728304ca95c3625e03
Nathan Tipper <n.tipper@uleth.ca>
2017-02-24 17:54:07 -0700

    Changed CouncilXMLParser so it no longer usesd Exceptions.h and uses the exception library.

--- src/CouncilXMLParser.cpp
+++ src/CouncilXMLParser.cpp
@@ -1,11 +1,12 @@
 #include "CouncilXMLParser.h"
-#include "Exceptions.h"
+#include <exception>
 #include <iostream>
 #include <stdio.h>
 
 using std::cout;
 using std::endl;
 using std::ios_base;
+using std::runtime_error;
 
 CouncilXMLParser::CouncilXMLParser(string filePath)
 {
