07d7ffe42368f26a42aa8c6cf062adff9cc96bac
Vincent <19965014+vincentcote87@users.noreply.github.com>
2017-02-24 17:34:38 -0700

    started UI class files

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,20 @@ using std::endl;
 int main()
 {
     cout << "Hello, world" << endl;
-    return 0;
+
+     string text, a, b, c, d;
+    text = "This is a test for the body This ihis is a test for the body This ihis is a test for the body This ihis is a test for the body This ihis i\
+s a test for the body This ihis is a test for the body This ihis is a test for the body This i";
+    a = "This is action one";
+    b = "This is action two";
+    c = "This is action three";
+    d = "This is action four";
+    vector<string> actn;
+    actn.push_back(a);
+    actn.push_back(b);
+    actn.push_back(c);
+    actn.push_back(d);
+
+    UI UITest(text, actn);
+    UITest.printUI();
 }
