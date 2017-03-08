c147f3afde7c7b6e8ebc5d04e3afd5194498a57a
Vincent <19965014+vincentcote87@users.noreply.github.com>
2017-03-07 19:20:08 -0700

    Finished testing files, ready to submit for pull request

--- include/TextBox.h
+++ include/TextBox.h
@@ -7,19 +7,51 @@
 using namespace std;
 
 const int COLUMN = 60;
-
+/**
+*Formats a string into a text box with borders
+*/
 class TextBox //: public Window
 {
 public:
+    /**
+    *Default constructor
+    *@param a string to be in the text box
+    */
     TextBox(std::string);
+    /**
+    *Deconstructor for TextBox
+    */
     virtual ~TextBox();
+    /**
+    *Copy constructor for TextBox
+    */
     TextBox(TextBox&);
+    /**
+    *Helper function to make a deep copy
+    *@param a TextBox to be copied
+    */
     void copy(TextBox&);
+    /**
+    *Overloaded assignment operator
+    */
     void operator=(TextBox&);
+    /**
+    *Prints the formated TextBox
+    *@param an ostream to print the TextBox
+    */
     void print(ostream&) const;
+    /**
+    *Overloaded insertion operator, uses print for format
+    */
     friend ostream& operator<<(ostream&, const TextBox&);
 
 private:
+    /**
+    *Helper function to print consecutive characters a desired amount of times
+    *@param int of desired consicutive chars
+    *@param the char to be printed
+    *@param ostream to print the characters
+    */
     void fillChar(int, char, ostream&) const;
 
     char ** textBody;
--- include/Window.h
+++ include/Window.h
@@ -3,16 +3,45 @@
 #include "TextBox.h"
 #include "PrintList.h"
 #include <ostream>
-
+/**
+*Prints a TextBox object with the option of printing a list below
+*/
 class Window
 {
 public:
+    /**
+    *Default constructor for Window
+    *@param a TextBox to print
+    */
     Window(TextBox);
+    /**
+    *Default constructor for Windows
+    *@param a TextBox to print
+    *@param a PrintList to print
+    */
     Window(TextBox, PrintList);
+    /**
+    *Deconstructor for Window
+    */
     virtual ~Window();
+    /**
+    *Reassigns the TextBox
+    *@param a TextBox
+    */
     void setTextBox(TextBox);
+    /**
+    *Reassigns the PrintList
+    *@param a PrintList
+    */
     void setPlayerChoice(PrintList);
+    /**
+    *Prints the TextBox and PrintList(if any)
+    *@param an ostream to print
+    */
     void print(ostream&);
+    /**
+    *Overloaded insertion operator, uses the print function
+    */
     friend ostream& operator<<(ostream&, Window&);
 
 
