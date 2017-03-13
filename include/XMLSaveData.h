1da241b357fed58f5c0a88dfb653c025bdc58f8f
Vincent <19965014+vincentcote87@users.noreply.github.com>
2017-03-13 12:17:42 -0600

    reAdded saving functionality files

---
+++ include/XMLSaveData.h
@@ -0,0 +1,16 @@
+#ifndef XMLSAVEDATA_H
+#define XMLSAVEDATA_H
+#include "tinyxml2.h"
+#include "SaveData.h"
+using namespace tinyxml2;
+
+class XMLSaveData
+{
+public:
+    static bool saveData(SaveData);
+
+private:
+
+};
+
+#endif // XMLSAVEDATA_H
