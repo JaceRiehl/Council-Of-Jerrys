131d7ead4a2bf992c7923f84eca5a55ed66433ef
Nathan Tipper <n.tipper@uleth.ca>
2017-03-12 20:33:50 -0600

    Started work on Search functionality.

--- src/Search.cpp
+++ src/Search.cpp
@@ -1,14 +1,7 @@
 #include "Search.h"
 
-Search::Search() : CharacterAction() {}
-
-Search::Search(string name) : CharacterAction(name) {}
-
-Search::Search(string name, string owner) : CharacterAction(name, owner) {}
-
-Search::Search(string name, string owner, string subject) : CharacterAction(name, owner, subject) {}
-
-Search::Search(string name, string owner, string subject, string responseText) : CharacterAction(name, owner, subject)
+Search::Search(Character* actionOwner, string key, Room* room, string responseText, vector<string> actionConditions, vector<Item> actionItems)
+        : RoomAction(actionOwner, key, room, responseText, actionConditions, actionItems)
 {
     this->responseText = responseText;
 }
@@ -17,8 +10,10 @@ Search::~Search() {}
 
 bool Search::execute()
 {
-    // If PlayableCharacter
 
-    // Give the PlayableCharacter an Item if item is not empty.
-    // Print off response text.
+}
+
+bool Search::execute(vector<string> playerActions)
+{
+
 }
