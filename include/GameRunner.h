6d7947628c458444db2418a6b66f9568603bdffb
Nathan Tipper <n.tipper@uleth.ca>
2017-04-05 18:05:19 -0600

    Added some stuff to GameRunner

--- include/GameRunner.h
+++ include/GameRunner.h
@@ -1,8 +1,20 @@
 #ifndef GAMERUNNER_H
 #define GAMERUNNER_H
 
+#include "PlayableCharacter.h"
+#include "DataManager.h"
+
 class GameRunner
 {
+    public:
+        GameRunner();
+
+    private:
+        PlayableCharacter* mainChar;
+        DataManager* dManager;
+        string nextLevel;
+        void init();
+
 
 };
 
