41f0dc489fe2ce6e131370bd74b421e6e6736568
VincentCote87 <VincentCote87@localhost>
2017-04-07 23:27:03 -0600

    Final draft of the documents

--- include/ChangeRoom.h
+++ include/ChangeRoom.h
@@ -6,9 +6,25 @@
 class ChangeRoom : public RoomAction
 {
     public:
+        /**
+        @brief Constructor for the change room class
+        @pre Information about the character and the room is given
+        @post None
+        @return None
+        */
         ChangeRoom(Character*, string, Room*, map<string, string> = {}, vector<string> = {}, vector<Item> = {});
 
+        /**
+        @brief Used to execute changing of room
+        @param None
+        @return Bool
+        */
         bool execute();
+        /**
+        @brief Used to execute changing of room with player actions
+        @param playerAction
+        @return Bool
+        */
         bool execute(vector<string>);
 
     private:
